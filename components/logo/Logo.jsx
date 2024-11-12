f9afe4423a81cda578186791258eb3711f592b27
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-11-12 21:00:46 +0900

    Fixed images quality

--- components/logo/Logo.jsx
+++ components/logo/Logo.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import styles from "./logo.module.css";
-import Image from "next/image";
 
 const Logo = ({ src, width, height }) => {
   return (
     <div className={styles.logoContainer}>
-      <Image src={src} alt="" width={width} height={height} />
+      <img src={src} alt="" width={width} height={height} />
     </div>
   );
 };
