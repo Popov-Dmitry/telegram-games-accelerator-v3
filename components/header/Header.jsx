d6f11290c1ed6fad8a596db5daa03dbdb67e58f2
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-12-10 19:00:57 +0900

    Updated header

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -3,15 +3,16 @@ import styles from "./header.module.css";
 import Image from "next/image";
 import Sns from "@/components/sns/Sns";
 import Button from "@/components/button/Button";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
-        <div className={styles.logo}>
+        <Link href="/" className={styles.logo}>
           <Image src="/logo.svg" alt="" width={32} height={32} />
           <div className={styles.text}>Telegram Gaming</div>
-        </div>
+        </Link>
         <div className={styles.right}>
           <Sns />
           <Button
